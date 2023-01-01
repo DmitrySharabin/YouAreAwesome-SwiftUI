d7c9e854cf6a2c28ad55d6433fceedbdf611a380
Dmitry Sharabin <dmitrysharabin@gmail.com>
2023-01-01 14:18:45 +0100

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Dmitry Sharabin on 1.1.23..
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
